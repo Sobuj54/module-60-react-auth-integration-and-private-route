49483377a4610c394abe52d015817f8a09a3d81e
soubj <sobujahmed124@gmail.com>
2023-05-04 21:07:03 +0600

    new project with firebase,tailwind,daisyUI,react router

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxtuoMBTqMxd-cQfaUFkxo_xYtIsPbay0",
+  authDomain: "auth-firebase-context-ta-43a3b.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-43a3b",
+  storageBucket: "auth-firebase-context-ta-43a3b.appspot.com",
+  messagingSenderId: "431918334557",
+  appId: "1:431918334557:web:6def47db943b104d0083b4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
